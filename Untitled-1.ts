a9558c28b29a1f248f6f9ac93a4841bb6d63b612
Philip Rodrigues <philip.rodrigues.js@gmail.com>
2025-12-29 05:07:08 -0500

    opposite number

---
+++ Untitled-1.ts
@@ -0,0 +1,8 @@
+// Very simple, given a number, find its opposite (additive inverse).
+
+export function opposite(number: number): number {
+    // Your code here
+    return (number * -1)
+}
+
+opposite(-5)
